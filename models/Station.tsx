b3ba0bcbad80e1ca5d502aa112f69e394970c199
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-04-25 12:01:07 +0700

    feat: Fix Merge

--- models/Station.tsx
+++ models/Station.tsx
@@ -2,7 +2,6 @@ import Realm, { BSON, ObjectSchema } from "realm";
 import {OpeningHours} from "./OpeningHours";
 import {Geometry} from "./Geometry";
 
-<<<<<<< HEAD
 interface Place {
     _id: string;
     formattedAddress: string;
@@ -12,9 +11,6 @@ interface Place {
     rating: number;
 }
 
-
-=======
->>>>>>> ff13a8a25ebb1a2f844be7b346493f1e6ec1fc36
 export class Station extends Realm.Object {
 
     static primaryKey = '_id';
