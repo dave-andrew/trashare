affb858d07483dbea2c131d4604592a90a6bf8b0
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-04-29 13:49:27 +0700

    feat: Total Point

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,9 @@ import { useQuery, useRealm, useUser } from "@realm/react";
 import { useCallback, useContext, useEffect, useState } from "react";
 import { Task } from "../../models/Task";
 import { router } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { AdditionalInfoContext } from "../providers/AdditionalInfoProvider";
+import { DashboardRoundedGrayBox } from "../../component/container/DashboardRoundedGrayBox";
 
 export default function Home() {
 
@@ -34,17 +36,19 @@ export default function Home() {
     const userAdditionalInfo = useContext(AdditionalInfoContext)
 
     return (
-        <View>
-            <Image
-                className='absolute w-full h-[11vh] rounded-max mx-auto'
+        <View className="bg-white h-full">
+            <ImageBackground
+                className='w-full h-[11vh] mx-auto'
                 source={require('../../assets/backgrounds/RegisterBG.png')}
-            />
-            <View className="mt-[6vh] justify-center ml-4">
+                imageStyle={{ borderBottomLeftRadius: 24, borderBottomRightRadius: 24 }}>
                 {userAdditionalInfo &&
-                    <Text className="text-lg font-medium color-white">Hi, {userAdditionalInfo.username}</Text>
+                    <Text className="text-lg font-medium color-white mt-12 ml-6">Hi, {userAdditionalInfo.username}</Text>
                 }
+            </ImageBackground>
+            <View className="flex flex-row w-full">
+                <DashboardRoundedGrayBox customClass=" ml-4 mr-2" label={"Total Points"} point={1000} units={'pt'} icon={<FontAwesome size={28} name="database" color={'#656565'} />} />
+                <DashboardRoundedGrayBox customClass=" mr-4 ml-2" label={"Total Weights"} point={1000} units={'gr'} icon={<FontAwesome size={28} name="anchor" color={'#656565'} />} />
             </View>
-
             <Text className="mt-5">Home {userAdditionalInfo?.username}</Text>
             <Button title="try add new data" onPress={() => router.push("/seeder/StationSeeder")}></Button>
             <EditScreenInfo path="app/(tabs)/home.tsx" />
