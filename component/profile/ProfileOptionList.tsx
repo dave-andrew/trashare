84ffb6033e8b5f7e8eeeb23e2282ce7d604392dc
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-05-01 16:05:51 +0700

    bug-fix: import issue

--- component/profile/ProfileOptionList.tsx
+++ component/profile/ProfileOptionList.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { View } from "react-native";
 import { User } from "../../models/User";
 import ProfileOption from "./ProfileOption";
 
