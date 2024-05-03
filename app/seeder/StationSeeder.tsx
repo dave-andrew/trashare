659ec4def440bb8bf5ca8fbc3ee30141ff058de3
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-05-03 13:58:09 +0700

    feat: Station Image URL

--- app/seeder/StationSeeder.tsx
+++ app/seeder/StationSeeder.tsx
@@ -17,6 +17,11 @@ export default function StationSeeder() {
     }, [realm, stationList])
 
     const seedStations = async () => {
+        if (stationList.length > 0) {
+            console.log("Station already seeded");
+            return;
+        }
+
         const stations = [
             {
                 name: "Rekosistem Compostable Station",
@@ -27,7 +32,7 @@ export default function StationSeeder() {
                         lng: 106.810177
                     }
                 },
-                imageUrl: "",
+                imageUrl: "https://firebasestorage.googleapis.com/v0/b/trashare-3a2a9.appspot.com/o/profile-pictures%2Fstation-rekosistem-1.jpg?alt=media&token=83cb28de-66cc-4781-901a-30cea2590391",
                 openingHours: {
                     open: "08:00",
                     close: "17:00"
@@ -43,7 +48,7 @@ export default function StationSeeder() {
                         lng: 106.844341
                     }
                 },
-                imageUrl: "",
+                imageUrl: "https://firebasestorage.googleapis.com/v0/b/trashare-3a2a9.appspot.com/o/profile-pictures%2Fstation-foodcyle-2.png?alt=media&token=a14bfa5d-1730-40ae-9b9e-980fc27a0b0b",
                 openingHours: {
                     open: "07:00",
                     close: "15:00"
@@ -59,7 +64,7 @@ export default function StationSeeder() {
                         lng: 106.782222
                     }
                 },
-                imageUrl: "",
+                imageUrl: "https://firebasestorage.googleapis.com/v0/b/trashare-3a2a9.appspot.com/o/profile-pictures%2Fstation-rebricks-3.png?alt=media&token=2d8a3913-9c62-4b4e-b0db-5b59da6b6925",
                 openingHours: {
                     open: "08:30",
                     close: "19:00"
@@ -75,7 +80,7 @@ export default function StationSeeder() {
                         lng: 106.803111
                     }
                 },
-                imageUrl: "",
+                imageUrl: "https://firebasestorage.googleapis.com/v0/b/trashare-3a2a9.appspot.com/o/profile-pictures%2Farah-environmental-4.png?alt=media&token=fdea6e12-b3b3-4db0-b1cc-29a097b70bba",
                 openingHours: {
                     open: "09:00",
                     close: "17:00"
@@ -91,6 +96,7 @@ export default function StationSeeder() {
                         lng: 115.216667
                     }
                 },
+                imageUrl: "https://firebasestorage.googleapis.com/v0/b/trashare-3a2a9.appspot.com/o/profile-pictures%2Fstation-ecopaper-5.png?alt=media&token=de5a3a5b-70dd-4d65-8623-0737f19a5a60",
                 openingHours: {
                     open: "12:00",
                     close: "15:00"
