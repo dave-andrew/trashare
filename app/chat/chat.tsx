61829a5296912bae8484cc72e68cf5c41b440984
Dep <109551168+dave-andrew@users.noreply.github.com>
2024-05-05 00:19:54 +0700

    feat: insert message to chat

--- app/chat/chat.tsx
+++ app/chat/chat.tsx
@@ -1,17 +1,19 @@
 import { useLocalSearchParams } from "expo-router";
-import { ImageBackground, Text } from "react-native";
+import { ImageBackground, Pressable, Text } from "react-native";
 import { getStationById, getUserChat } from "../datas/queries/useQueries";
 import { useChatMutation } from "../datas/mutations/useMutations";
 import { useRealm } from "@realm/react";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AdditionalInfoContext } from "../providers/AdditionalInfoProvider";
+import { FlatList } from "react-native-gesture-handler";
+import RoundedTextField from "../../component/form/RoundedTextField";
 
 export default function ChatPage() {
 
   const station_id = useLocalSearchParams().station
-  // console.log(station_id)
   const station = getStationById(station_id)
-  // console.log(station)
+
+  const [messageInput, setMessageInput] = useState<string>("")
 
   const realm = useRealm()
   const chat = getUserChat(station)
@@ -19,6 +21,15 @@ export default function ChatPage() {
   const { additionalInfo } = useContext(AdditionalInfoContext)
 
   const { createChat } = useChatMutation(realm, chat)
+  const { addMessage } = useChatMutation(realm, chat)
+
+  const handleAddMessage = () => {
+    addMessage(chat[0], {
+      text: messageInput,
+      user: additionalInfo
+    })
+    setMessageInput("")
+  }
 
   useEffect(() => {
     if (chat.length === 0) {
@@ -34,7 +45,27 @@ export default function ChatPage() {
     <ImageBackground
       source={require('../../assets/backgrounds/RegisterBG.png')}
       style={{ width: '100%', height: '100%' }}>
-        
+
+      <FlatList
+        data={chat[0].message}
+        renderItem={({ item }) => {
+          return (
+            <Text>{item.text}</Text>
+          )
+        }}
+      >
+      </FlatList>
+
+      <RoundedTextField
+        placeholder="Type a message"
+        onChangeFunction={(text) => setMessageInput(text)}
+        value={messageInput}
+      />
+
+      <Pressable onPress={handleAddMessage}>
+        <Text>Send</Text>
+      </Pressable>
+
     </ImageBackground>
   )
 }
