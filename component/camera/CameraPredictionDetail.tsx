9c9ff5f22a8df1fb58d52d089cfa49f5d82d00b8
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-05-05 23:35:31 +0700

    feat: Prediction Sentence

--- component/camera/CameraPredictionDetail.tsx
+++ component/camera/CameraPredictionDetail.tsx
@@ -6,6 +6,43 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 
 export default function CameraPredictionDetail(props: { prediction: string, imageUrl: string, setPageState: Dispatch<React.SetStateAction<string>> }) {
+    const classesList = {
+        'cardboard': {
+            "category": "Paper",
+            "icon-name": "sticky-note",
+            "shortDescription": "Cardboard is a generic term for heavy-duty paper-based products having greater thickness and superior durability or other specific mechanical attributes to paper;"
+        },
+        'glass': {
+            "category": "Hazardous",
+            "icon-name": "exclamation-triangle",
+            "shortDescription": "Glass is a non-crystalline, often transparent amorphous solid, that has widespread practical, technological, and decorative use in, for example, window panes, tableware, and optics. "
+        },
+        'metal': {
+            "category": "Hazardous",
+            "icon-name": "exclamation-triangle",
+            "shortDescription": "Metal is a material that, when freshly prepared, polished, or fractured, shows a lustrous appearance, and conducts electricity and heat relatively well."
+        },
+        'paper': {
+            "category": "Paper",
+            "icon-name": "sticky-note",
+            "shortDescription": "Paper is a thin sheet material produced by mechanically and/or chemically processing cellulose fibres derived from wood, rags, grasses or other vegetable."
+        },
+        'plastic': {
+            "category": "Recyclable",
+            "icon-name": "recycle",
+            "shortDescription": "Cardboard is a generic term for heavy-duty paper-based products having greater thickness and superior durability or other specific mechanical attributes to paper; such as foldability, rigidity and impact resistance. The construction can range from a thick sheet known as paperboard to corrugated fiberboard which is made of multiple corrugated and flat layers."
+        },
+        'trash': {
+            "category": "Compost",
+            "icon-name": "leaf",
+            "shortDescription": "TODO: Diskusi trash itu apa"
+        },
+        'biological': {
+            "category": "Compost",
+            "icon-name": "leaf",
+            "shortDescription": "Biological waste is any organic waste that can be composted, such as food waste, yard waste, and animal waste."
+        },
+    }
     return (
         <BottomSheet
             style={{ elevation: 5, paddingHorizontal: 32, gap: 10 }}
