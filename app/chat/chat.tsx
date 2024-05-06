614a7ca9d9b264b53d086f31578b50eed6ce97ed
Dep <109551168+dave-andrew@users.noreply.github.com>
2024-05-06 08:40:43 +0700

    feat: bubble chat

--- app/chat/chat.tsx
+++ app/chat/chat.tsx
@@ -1,12 +1,13 @@
 import { useLocalSearchParams } from "expo-router";
-import { ImageBackground, Pressable, Text } from "react-native";
+import { ImageBackground, Pressable, Text, View } from "react-native";
 import { getStationById, getUserChat } from "../datas/queries/useQueries";
 import { useChatMutation } from "../datas/mutations/useMutations";
 import { useRealm } from "@realm/react";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { AdditionalInfoContext } from "../providers/AdditionalInfoProvider";
 import { FlatList } from "react-native-gesture-handler";
 import RoundedTextField from "../../component/form/RoundedTextField";
+import ChatBubble from 'react-native-chat-bubble';
 
 export default function ChatPage() {
 
@@ -15,6 +16,7 @@ export default function ChatPage() {
   const station = getStationById(realm, station_id)
 
   const [messageInput, setMessageInput] = useState<string>("")
+  const flatListRef = useRef<FlatList>(null);
 
   const chat = getUserChat(station)
   console.log(chat)
@@ -41,23 +43,38 @@ export default function ChatPage() {
     }
   }, [chat])
 
+  useEffect(() => {
+    if (flatListRef.current) {
+      flatListRef.current.scrollToEnd({ animated: true });
+    }
+  }, [chat]);
+
   return (
     <ImageBackground
       source={require('../../assets/backgrounds/RegisterBG.png')}
       style={{ width: '100%', height: '100%' }}>
 
       <FlatList
+        ref={flatListRef}
+        className="py-2 px-4"
         data={chat[0].message}
         renderItem={({ item }) => {
-          return (
-            <Text>{item.text}</Text>
+          return(
+            <ChatBubble
+              isOwnMessage={item.user._id === additionalInfo._id}
+              bubbleColor="#8CE7FF"
+              tailColor="#8CE7FF"
+              withTail={true}
+            >
+              <Text className="text-base">{item.text}</Text>
+            </ChatBubble>
           )
         }}
       >
       </FlatList>
 
       <RoundedTextField
-        placeholder="Type a message"
+        placeholder="Type a message..."
         onChangeFunction={(text) => setMessageInput(text)}
         value={messageInput}
       />
