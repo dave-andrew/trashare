b6c085ddb2c99d30a36d0723d147266d8689a578
Dep <109551168+dave-andrew@users.noreply.github.com>
2024-05-07 09:01:24 +0700

    feat: cha

--- component/form/RoundedTextField.tsx
+++ component/form/RoundedTextField.tsx
@@ -13,4 +13,18 @@ const RoundedTextField = ({value, onChangeFunction, placeholder, secureTextEntry
     )
 }
 
+export const ChatTextField = ({value, onChangeFunction, placeholder, secureTextEntry= false, onSubmitEditing}) => {
+    return (
+        <TextInput
+            className={"rounded-full border border-gray-400 flex-1 py-1 px-4 my-2 mx-4"}
+            placeholder={placeholder}
+            value={value}
+            onChangeText={onChangeFunction}
+            secureTextEntry={secureTextEntry}
+            onSubmitEditing={onSubmitEditing}
+        />
+    )
+}
+
+
 export default RoundedTextField;
